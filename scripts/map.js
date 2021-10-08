31a7dc5915379169567e5b4df5a3b7d8a508c830
incrediblemop <91903187+incrediblemop@users.noreply.github.com>
2021-10-08 17:15:05 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,7 +130,9 @@ $(window).on('load', function() {
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
           point['Description']+ '</b><br>' +
-          point['Location']);
+          point['Location']+ '</b><br>' +
+          point['Date Started']+ '</b><br>' +
+          point['Date Ended']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
