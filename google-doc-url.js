4934ab1d37bdc25ad45f75209a58674ae81ae26e
DiploTraining <91903187+DiploTraining@users.noreply.github.com>
2021-10-15 14:27:43 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yir1oyKis1fqd0910KhKZBvZUkxIVNXbtTeUfKCx5VA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBQRD9wq1ZBi9vBzEu4WR55m1C7Ah0cIaw';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
